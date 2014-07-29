b7de7ce03184c2b182eb3e32e81f4467c2490493
philopon <philopon.dependence@gmail.com>
2014-07-30 00:02:12 +0900

    add examples of encode.

--- examples/Simple.purs
+++ examples/Simple.purs
@@ -1,4 +1,4 @@
-module Simple where
+module Main where
 
 import Control.Monad.Eff
 
@@ -11,6 +11,7 @@ import qualified Data.Set as S
 
 main :: Eff (trace :: Debug.Trace.Trace) Unit
 main = do
+    Debug.Trace.trace "decode"
     Debug.Trace.print (decode "12"      :: Maybe Number)
     Debug.Trace.print (decode "\"foo\"" :: Maybe String)
     Debug.Trace.print (decode "true"    :: Maybe Boolean)
@@ -19,10 +20,27 @@ main = do
     Debug.Trace.print (decode "[1,\"Number\"]" :: Maybe (Tuple Number String))
     Debug.Trace.print (decode "[1,2,3,2]" :: Maybe [Number])
     Debug.Trace.print (decode "[1,2,3,2]" :: Maybe (S.Set Number))
-    Debug.Trace.print (eitherDecode "{\"foo\": 1, \"bar\": 2}" :: Either String (M.Map String Number))
+    Debug.Trace.print (decode "{\"foo\": 1, \"bar\": 2}" :: Maybe (M.Map String Number))
 
     Debug.Trace.print (decode "12" :: Maybe (Maybe String))
     Debug.Trace.print (decode "{\"Left\": true}" :: Maybe (Either Boolean Number))
 
 
     Debug.Trace.print (decode "[{\"foo\": [1,2,3]}, true]" :: Maybe Value)
+
+    Debug.Trace.trace "\nencode"
+    Debug.Trace.trace (encode 12)
+    Debug.Trace.trace (encode "foo")
+    Debug.Trace.trace (encode true)
+    Debug.Trace.trace (encode unit)
+
+    Debug.Trace.trace (encode $ Tuple 1 "Number")
+    Debug.Trace.trace (encode $ [1,2,3,2])
+    Debug.Trace.trace (encode $ S.fromList [1,2,3,2])
+    Debug.Trace.trace (encode $ M.fromList [Tuple "foo" 1, Tuple "bar" 2])
+
+    Debug.Trace.trace (encode (Just 12))
+    Debug.Trace.trace (encode (Left true :: Either Boolean Number))
+
+    Debug.Trace.trace (encode $ Array [object ["foo" .= Array [Number 1,Number 2,Number 3]], Bool true])
+
